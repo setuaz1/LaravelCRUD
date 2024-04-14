7c0f7335da604fe930fd8b2c1d238d7614c32521
setuaz1 <paparap@gmail.com>
2024-04-14 13:43:59 +0300

    28 lesson

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
+
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +17,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Category::factory(20)->create();
+        $tags = Tag::factory(50)->create();
+        $posts = Post::factory(200)->create();
+
+        foreach ($posts as $post) {
+            $tagsIds = $tags->random(5)->pluck('id');
+            $post->tags()->attach($tagsIds);
+        }
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
     }
 }
