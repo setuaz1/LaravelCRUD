bcf131e41086aff0f43c18635d521cc6348a0099
setuaz1 <paparap@gmail.com>
2024-04-15 11:01:26 +0300

    29 lesson

--- app/Http/Controllers/Post/IndexController.php
+++ app/Http/Controllers/Post/IndexController.php
@@ -8,7 +8,7 @@ class IndexController extends BaseController
 {
     public function __invoke()
     {
-        $posts = Post::all();
+        $posts = Post::paginate(10);
         return view('post.index', compact('posts'));
     }
 }
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.main')
 @section('content')
-<div>
-  <div>
-    <a href="{{ route('post.create') }}" class="btn btn-outline-primary mb-3">Add one</a>
-  </div>
-  @foreach($posts as $post)
-  <div><a href="{{route('post.show', $post->id) }}">{{ $post->id }}. {{ $post->title }}</a></div>
-  @endforeach
-</div>
+    <div>
+        <div>
+            <a href="{{ route('post.create') }}" class="btn btn-outline-primary mb-3">Add one</a>
+        </div>
+        @foreach($posts as $post)
+            <div><a href="{{route('post.show', $post->id) }}">{{ $post->id }}. {{ $post->title }}</a></div>
+        @endforeach
+        <div class="mt-3">
+            {{ $posts->links() }}
+        </div>
+    </div>
 @endsection
