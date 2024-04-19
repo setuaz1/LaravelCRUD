e64c4e6a662485a2f79c9d0d91813d9454f30c1b
setuaz1 <paparap@gmail.com>
2024-04-19 13:46:30 +0300

    33 lesson

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'AdminPanelMiddleware' => \App\Http\Middleware\AdminPanelMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\HomeController@index');
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home');
 
 Route::group(['namespace'=>'App\Http\Controllers\Post'], function () {
     Route::get('/posts', 'IndexController')->name('post.index');
@@ -18,7 +18,7 @@ Route::group(['namespace'=>'App\Http\Controllers\Post'], function () {
 
 });
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function() {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => 'AdminPanelMiddleware'], function() {
     Route::group(['namespace' => 'Post'], function() {
         Route::get('/post', 'IndexController')->name('admin.post.index');
     });
